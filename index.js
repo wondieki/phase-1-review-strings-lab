a6d110e639333f808e7e163a661f521a2c2b67c4
wondieki <wkerubo28@gmail.com>
2024-11-04 11:35:05 +0300

    Done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-const currentUser = 'Grace Hopper';
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// Write your code in this file!
+const currentUser='Donvine Mugendi'
+const welcomeMessage="Welcome to Flatbook, "+ currentUser+'!';
+const excitedWelcomeMessage= welcomeMessage.toUpperCase()
+const firstInitial=currentUser.slice(0,1);
+const shortGreeting='Welcome, '+ firstInitial+ '!'
