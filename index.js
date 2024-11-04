4a16db92280e777fb9dc0e58876164b5d254f546
wondieki <wkerubo28@gmail.com>
2024-11-04 11:40:55 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser='Donvine Mugendi'
+const currentUser='Wilder Ondieki'
 const welcomeMessage="Welcome to Flatbook, "+ currentUser+'!';
 const excitedWelcomeMessage= welcomeMessage.toUpperCase()
 const firstInitial=currentUser.slice(0,1);
